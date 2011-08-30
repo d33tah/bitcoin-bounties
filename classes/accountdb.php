9ecafec499ee7d1ccd24c1e8adaa529e3caf8ee7
d33tah <d33tah@gmail.com>
2011-08-30 14:29:01 +0200

    added voting, no payouts yet

---
+++ classes/accountdb.php
@@ -0,0 +1,38 @@
+<?php
+
+class AccountDB {
+
+var $conn;
+
+public function __construct($login,$pass,$host,$port,$path) {
+        require_once(ROOT.'/classes/jsonRPCClient.php');
+	$this->conn = new jsonRPCClient
+          (sprintf('http://%s:%s@%s:%s/%s',$login,$pass,$host,$port,$path));
+
+}
+
+public function create_address($label='')
+{
+  if($label)
+    return $this->conn->getnewaddress($label);
+  else
+    return $this->conn->getnewaddress();
+}
+
+public function balance_prefix($prefix)
+{
+  $ret = 0;
+  $result = $this->conn->listaccounts();
+  //print_r($result);
+  foreach($result as $key => $value)
+    if(strpos($key,$prefix)===0)
+      $ret+=round($value,8)*1e8;
+  return $ret/pow(10,8);
+}
+
+public function balance_address($address)
+{
+  return $this->conn->getbalance($address);
+}
+
+}
--- classes/bountydb.php
+++ classes/bountydb.php
@@ -41,17 +41,22 @@ public function get_submissions($bounty)
   return $ret;
 }
 
-public function add_bounty($title,$desc,$uid)
+public function add_bounty($title,$desc,$uid,$accounts_db)
 {
   $title_safe=mysql_real_escape_string($title);
   $desc_safe=mysql_real_escape_string($desc);
   $uid_safe=mysql_real_escape_string($uid);
   $sql = "INSERT INTO `bounties` (`id`,`title`,`user_id`,`description`,
-    `bitcoins`,`satoshi`) VALUES ('','".$title_safe."','".$uid_safe.
-    "','".$desc_safe."','0','0')";
+    `bitcoins`,`satoshi`, `address`) VALUES ('','".$title_safe."','".$uid_safe.
+    "','".$desc_safe."','0','0','')";
   mysql_query($sql);
   echo mysql_error();
-  return mysql_insert_id();
+  if(mysql_insert_id())
+  {
+    $new_address = $accounts_db->create_address('bounty_'.mysql_insert_id());
+    $sql=mysql_query("UPDATE `bounties` SET `address`='$new_address'");
+    mysql_query($sql);
+  }
 }
 
 public function get_submission($id)
@@ -79,5 +84,111 @@ public function add_submission($bounty_id,$uid,$desc,$filename)
   return mysql_insert_id();
 }
 
+
+public function get_address($bounty,$user_id, $accounts_db)
+{
+  $id_safe=mysql_real_escape_string($bounty['id']);
+  $uid_safe=mysql_real_escape_string($user_id);
+  $sql = "SELECT * FROM `accounts` WHERE `bounty_id`='".$id_safe."' AND
+    `user_id`='".$uid_safe."'";
+  if($res = mysql_query($sql))
+  {
+    if($ret=mysql_fetch_assoc($res))
+    {
+      return $ret['address'];
+    }
+  }
+
+  $new_address = $accounts_db->create_address('bounty_'.$id_safe.'_'
+    .$uid_safe);
+  $new_address_safe=mysql_real_escape_string($new_address);
+  $sql = "INSERT INTO `accounts` (`bounty_id`,`user_id`,`address`) VALUES (
+    '$id_safe', '$uid_safe', '$new_address_safe')";
+  mysql_query($sql);
+  echo mysql_error();
+  return $new_address;
+}
+
+public function getvotes_commit($commit_id,$accounts_db)
+{
+  $commitid_safe=mysql_real_escape_string($commit_id);
+  $submission=$this->get_submission($commitid_safe);
+  $bounty=$this->get_by_id($submission['bounty_id']);
+  $total=$accounts_db->balance_prefix('bounty_'.$bounty['id']);
+  $total_anonymous=$accounts_db->balance_address($bounty['address']);
+  $total_by_registered=$total-$total_anonymous;
+  if($total_by_registered)
+  {
+    $total_voted=0;
+    $sql = "SELECT * FROM `votes` WHERE `commit_id`='".$commitid_safe."'";
+    $res = mysql_query($sql);
+    if($res)
+    {
+      while($row = mysql_fetch_assoc($res))
+      {
+	$total_voted+=$accounts_db->balance_address(
+          'bounty_'.$bounty['id'].'_'.$row['user_id']);
+      }
+    }
+  }
+
+  if (false)
+  print "total=$total, total_anonymous=$total_anonymous,
+   total_by_registered=$total_by_registered, total_voted=$total_voted\n<br/>";
+
+  return $total_voted/$total_by_registered*100;
+}
+
+public function voteup_commit($commit_id,$accounts_db,$user_db,$user_id)
+{
+  global $messages, $LINK_PREFIX, $domain, $mail;
+
+  $commitid_safe=mysql_real_escape_string($commit_id);
+  $uid_safe=mysql_real_escape_string($user_id);
+  $submission=$this->get_submission($commitid_safe);
+  $bounty=$this->get_by_id($submission['bounty_id']);
+  $sql = "SELECT * FROM `votes` WHERE `commit_id`='".$commitid_safe."' AND 
+    `user_id`='".$uid_safe."'";
+  $res=mysql_query($sql);
+  echo mysql_error();
+  if($res)
+  {
+    $row=mysql_fetch_assoc($res);
+    if($row)
+    {
+      return false;
+    }
+  }
+
+  $sql = "INSERT INTO `votes` (`id`,`commit_id`,`user_id`,`vote_time`) VALUES
+    ('','".$commitid_safe."','".$uid_safe."','".time()."')";
+  mysql_query($sql);
+
+  if($this->getvotes_commit($commitid_safe,$accounts_db)>30)
+  {
+    $total=$accounts_db->balance_prefix('bounty_'.$bounty['id']);
+    $fee=$total*0.01;
+    
+    $commit=$this->get_submission($commitid_safe);
+    $receiving_user=$user_db->get_by_id($commit['user_id']);
+
+    $bounty_gathered_mail=new Template($messages[MSG_BOUNTY_GATHERED]);
+    $bounty_gathered_mail->replace('DOMAIN',$domain);
+    $bounty_gathered_mail->replace('USERNAME',$receiving_user['login']);
+    $bounty_gathered_mail->replace('BOUNTYID',$bounty['id']);
+    $bounty_gathered_mail->replace('VALUE',sprintf("%.8f",$total-$fee));
+    $bounty_gathered_mail->replace('LINK_PREFIX',$LINK_PREFIX);
+
+    $mail->Body = $bounty_gathered_mail->get_body();
+    $mail->Subject = $messages[MSG_BOUNTY_GATHERED_TITLE];
+    $mail->AddAddress($receiving_user['mail'],$receiving_user['login']);
+
+    $result = $mail->Send();
+    $mail->ClearAddresses();
+    $mail->ClearAttachments();
+
+  }
+  return true;
+}
 
 }
