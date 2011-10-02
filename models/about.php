325b98fce3f81ed8626ddfcb0060af938e66a6c4
d33tah <d33tah@gmail.com>
2011-10-02 13:55:15 +0200

    added a about/FAQ entry

--- models/about.php
+++ models/about.php
@@ -2,5 +2,4 @@
 $title=$domain.' - '.__(MSG_BOUNTY_SYSTEM_RULES);
 $tpl->TITLE=$title;
 $tpl->SHORT_TITLE=$title;
-$tpl->MESSAGE="Here will be some 
-decent rules and FAQ or something in the future."; //TODO!
+$tpl->MESSAGE=__(MSG_ABOUT); //TODO!
